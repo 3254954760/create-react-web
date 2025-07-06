cb8cdc4e4aaebd93d9b439802c062ed15a5dd110
liyong01 <liyong01@dp.tech>
2025-07-06 21:39:50 +0800

    受不了webpack换成vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,25 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src') // 可选别名
+            '@': path.resolve(__dirname, './src'), // 可选别名
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@router': path.resolve(__dirname, './src/router'),
+            '@layout': path.resolve(__dirname, './src/layout'),
+            '@model': path.resolve(__dirname, './src/model'),
+            '@service': path.resolve(__dirname, './src/service')
         },
         extensions: ['.js', '.ts', '.jsx', '.tsx']
     },
+
     css: {
         preprocessorOptions: {
             less: {
                 javascriptEnabled: true // 如果你用到 antd、变量嵌套之类的 less 特性
             }
         }
+    },
+
+    server: {
+        port: 5000
     }
 });
