e5fab084602fd70d884fe3a4985a7ffc0284011a
liyong01 <liyong01@dp.tech>
2025-09-07 14:44:30 +0800

    react 试验

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 const Home = () => {
     const history = useHistory();
-
+    const [state, setState] = useState(0);
     const goLogin = () => {
         history.push('/login');
     };
+    useEffect(() => {
+        console.log('创建', state);
+        return () => {
+            console.log('销毁', state);
+        };
+    }, [state]);
     return (
         <>
             <div>home</div>
+
+            <div onClick={() => setState(state + 1)}>销毁重建</div>
             <button onClick={goLogin}>去登录</button>
         </>
     );
