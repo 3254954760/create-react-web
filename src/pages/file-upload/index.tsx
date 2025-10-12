62b823c81049595369cd4127bcc072fb33fa27c3
liyong01 <liyong01@dp.tech>
2025-10-12 16:29:04 +0800

    feat: 文件分片上传和合并

--- src/pages/file-upload/index.tsx
+++ src/pages/file-upload/index.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+import { Button, Input } from 'antd';
+import { useCallback, useState } from 'react';
 const UPLOAD_CHUNK_SIZE = 1024 * 10; // 10KB
 const FileUpload = () => {
     const [file, setFile] = useState<File | null>(null);
+    const [canMerge, setCanMerge] = useState(false);
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
         setFile(file as File);
-        const chunks = sliceFile(file as File, UPLOAD_CHUNK_SIZE);
-        uploadFile(chunks as Blob[]);
     };
     const sliceFile = (file: File, size: number = UPLOAD_CHUNK_SIZE) => {
         const chunks = [];
@@ -15,36 +15,46 @@ const FileUpload = () => {
         }
         return chunks;
     };
-    const uploadFile = (chunks: Blob[]) => {
+    const handleUploadFile = useCallback(() => {
+        const chunks = sliceFile(file as File, UPLOAD_CHUNK_SIZE);
         const listFetch = chunks.map((chunk, index) => {
             const formData = new FormData();
-            formData.append('file', chunk);
             formData.append('filename', file?.name || '');
             formData.append('index', index.toString());
             formData.append('total', chunks.length.toString());
-            return fetch('/api/upload', {
+            // 这一句一定要写在最后面，应为multer中间件会优先解析file，解析了file之后，formData.append('file', chunk);就解析不了了
+            formData.append('file', chunk);
+            return fetch('/api/upload-file', {
                 method: 'POST',
                 body: formData
             });
         });
         Promise.all(listFetch).then((res: any) => {
-            fetch('/api/merge', {
-                method: 'POST',
-                body: JSON.stringify({
-                    filename: file?.name,
-                    total: chunks.length
-                })
-            }).then((res: any) => {
-                console.log(res);
-            });
+            setCanMerge(true);
+        });
+    }, [file]);
+
+    const handleMerge = () => {
+        console.log('合并文件');
+        fetch('/api/merge-file', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                filename: file?.name
+            })
+        }).then((res: any) => {
+            console.log(res);
         });
     };
 
     return (
         <>
             <div>FileUpload</div>
-            <input type="file" onChange={handleFileChange} />
-            <button>上传</button>
+            <Input type="file" onChange={handleFileChange} />
+            <Button onClick={handleUploadFile}>上传</Button>
+            {canMerge && <Button onClick={handleMerge}>合并文件</Button>}
         </>
     );
 };
